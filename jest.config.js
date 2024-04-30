6da359197c88e6e91d5588ab3eb90d9fcc46bc99
YashmeetSAP <yashmeet@sap.com>
2024-04-30 16:20:08 +0530

    Update package.json & separate db calls

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,9 @@
 const config = {
   verbose: true,
   testTimeout: 100000,
-  testMatch: ['**/test/*.test.js'],
-  collectCoverageFrom: [
-    '**/lib/*',
-    '**/srv/*',
-    '**/cds-plugin.js'
-  ],
-  coverageReporters: ['lcov', 'text', 'text-summary'],
+  testMatch: ["**/test/**/*.test.js"],
+  collectCoverageFrom: ["**/lib/*", "**/srv/*", "**/cds-plugin.js"],
+  coverageReporters: ["lcov", "text", "text-summary"],
 };
 
 module.exports = config;
