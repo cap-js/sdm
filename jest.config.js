7352c2da8f05d45e1594d8b1acd0a9ebf4fa486b
YashmeetSAP <yashmeet@sap.com>
2024-05-29 14:10:43 +0530

    Fix folder deletion when entity without having any attachments is deleted

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 const config = {
   verbose: true,
   testTimeout: 100000,
-  testMatch: ["**/test/**/*.test.js"],
+  testMatch: ["**/test/lib/**/*.test.js"],
   collectCoverageFrom: ["**/lib/**/*"],
   coveragePathIgnorePatterns: ["node_modules", "<rootDir>/lib/persistence"],
   coverageReporters: ["lcov", "text", "text-summary"],
 };
- 
+
 module.exports = config;
