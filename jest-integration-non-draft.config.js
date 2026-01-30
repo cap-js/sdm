62f2ca3cd6c45d9b6c5481da3b11c65e14c35d2c
Yashmeet . <yashmeet@sap.com>
2026-01-30 15:22:49 +0530

    Non-draft support & integration tests

---
+++ jest-integration-non-draft.config.js
@@ -0,0 +1,7 @@
+const integrationNonDraftConfig = {
+  verbose: true,
+  testTimeout: 100000,
+  testMatch: ["**/test/integration/attachments-sdm-non-draft.test.js"]
+};
+
+module.exports = integrationNonDraftConfig;
